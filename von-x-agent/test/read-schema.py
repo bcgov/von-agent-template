494d3708a49207c8ed0c6e6449a9473ca673174d
Ian Costanzo <ian@anon-solutions.ca>
2018-11-25 17:40:43 -0800

    Sample meta-schema

--- von-x-agent/test/read-schema.py
+++ von-x-agent/test/read-schema.py
@@ -4,8 +4,15 @@ import yaml
 
 with open("config/schemas-meta.yml", 'r') as stream:
     try:
-        meta = yaml.load(stream) 
-        print(meta)
+        schemas = yaml.load(stream) 
+        print(schemas)
+        for schema in schemas:
+            print(schema['name'])
+            print(schema['version'])
+            print(schema['description'])
+            for attr in schema['attributes'].keys():
+                print(' ', attr)
+                print(' ', schema['attributes'][attr]['required'])
     except yaml.YAMLError as exc:
         print(exc)
 
