d17b7bbb7f66cd9cadb814fbdb357fbc2180f8b4
Ian Costanzo <ian@anon-solutions.ca>
2018-11-29 14:09:51 -0800

    Test data

--- von-x-agent/test/read-schema.py
+++ von-x-agent/test/read-schema.py
@@ -5,6 +5,10 @@ import argparse
 import os.path
 import os
 import sys
+import datetime
+import pytz
+import json
+
 
 # get name of input file from args
 
@@ -58,6 +62,31 @@ def get_option(list, prompt='-->', default=None, required=False, list_style=True
 def path_to_name(schema_path):
     return schema_path.replace('/', '')
 
+def now_date():
+    tz_aware = timezone.localize(datetime.datetime.now())
+    return tz_aware.astimezone(pytz.utc).isoformat()
+
+# ui_name, ui_address, ui_text, ui_date, ui_select, helper_uuid, helper_now_iso, helper_value
+def sample_data(attr_name, data_type):
+    if data_type == 'ui_name':
+        return 'Name'
+    elif data_type == 'ui_address':
+        return 'Address'
+    elif data_type == 'ui_text':
+        return 'Text'
+    elif data_type == 'ui_date':
+        return now_date()
+    elif data_type == 'ui_select':
+        return 'Select'
+    elif data_type == 'helper_uuid':
+        return 'UUID'
+    elif data_type == 'helper_now_iso':
+        return now_date()
+    elif data_type == 'helper_value':
+        return 'Value'
+    else:
+        return 'Unknown type'
+
 #Using a custom Dumper class to prevent changing the global state
 class CustomDumper(yaml.Dumper):
     #Super neat hack to preserve the mapping key order. See https://stackoverflow.com/a/52621703/1497385
@@ -70,10 +99,14 @@ if 2 > len(sys.argv):
     print("usage:", sys.argv[0], "<input file>")
     sys.exit()
 
+# for now, we are in PST time
+timezone = pytz.timezone("America/Los_Angeles")
+
 in_file = sys.argv[1]
 in_dir = os.path.dirname(in_file)
 out_services = in_dir + '/gen-services.yml'
 out_routes = in_dir + '/gen-routes.yml'
+out_data = in_dir + '/gen-data.json'
 
 with open(in_file, 'r') as stream:
     try:
@@ -96,6 +129,7 @@ with open(in_file, 'r') as stream:
         services = {}
         services['credential_types'] = []
         routes = {}
+        testdata = []
         for schema in schemas:
             # generate schema-level stuff for services.yml
             service = {}
@@ -255,6 +289,14 @@ with open(in_file, 'r') as stream:
                             attribute['source'] = schema['attributes'][attr]['data_type']
                     routes[form_name]['mappings']['attributes'].append(attribute)
 
+            datapacket = {}
+            datapacket['schema'] = schema['name']
+            datapacket['version'] = schema['version']
+            datapacket['attributes'] = {}
+            for attr in schema['attributes'].keys():
+                datapacket['attributes'][attr] = sample_data(attr, schema['attributes'][attr]['data_type'])
+            testdata.append(datapacket)
+
         #y_schemas = yaml.dump(schemas, default_flow_style=False, Dumper=CustomDumper)
         #print(y_schemas)
 
@@ -269,6 +311,10 @@ with open(in_file, 'r') as stream:
         print('Writing', out_routes)
         with open(out_routes, 'w') as outfile:
             yaml.dump(routes, outfile, default_flow_style=False, Dumper=CustomDumper)
+
+        print('Writing', out_data)
+        with open(out_data, 'w') as outfile:
+            outfile.write(json.dumps(testdata, indent=4, sort_keys=True))
     except yaml.YAMLError as exc:
         print(exc)
 
