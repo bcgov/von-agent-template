302a308326b779c6f992cb5d9a798cbafb34b506
Ian Costanzo <ian@anon-solutions.ca>
2018-12-02 13:02:28 -0800

    Script to initialize configurations

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 export MY_ORG=ianco-org
 export MY_SEED=bc_von_x_dev_0000000000000000666
+export MY_DID=someBigHexString
 
 find von-x-agent/config -name "*.yml" -exec sed -i s/my-organization/$MY_ORG/g {} +
+find von-x-agent/config -name "*.yml" -exec sed -i s/8HkrLGCUemkvspADWnJu4e/$MY_SEED/g {} +
 sed -i s/bc_von_x_dev_0000000000000000000/$MY_SEED/g von-x-agent/config/settings.yml
